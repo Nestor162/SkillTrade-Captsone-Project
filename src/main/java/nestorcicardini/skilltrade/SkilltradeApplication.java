81c5281269f64668857961a0c692a0f2904b1b6d
Nestor162 <nestorcicardini@gmail.com>
2023-06-27 12:19:38 +0200

    Add initial project files

---
+++ src/main/java/nestorcicardini/skilltrade/SkilltradeApplication.java
@@ -0,0 +1,13 @@
+package nestorcicardini.skilltrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilltradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkilltradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nestorcicardini/skilltrade/SkilltradeApplicationTests.java
@@ -0,0 +1,13 @@
+package nestorcicardini.skilltrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilltradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
