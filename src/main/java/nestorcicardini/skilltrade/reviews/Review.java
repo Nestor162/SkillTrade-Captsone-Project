4f7ab7473ea3dc8b1e9ac9f1ae645ffe18170126
Nestor162 <nestorcicardini@gmail.com>
2023-06-27 18:51:08 +0200

    Create Review Class

---
+++ src/main/java/nestorcicardini/skilltrade/reviews/Review.java
@@ -0,0 +1,38 @@
+package nestorcicardini.skilltrade.reviews;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import nestorcicardini.skilltrade.profiles.Profile;
+
+@Entity
+@Table(name = "reviews")
+@Getter
+@Setter
+public class Review {
+	@Id
+	@GeneratedValue
+	private UUID id;
+	private String title;
+	private String content;
+	private LocalDate publicationDate;
+	private int likes;
+	private int rating;
+
+	@ManyToOne
+	@JoinColumn(name = "review_author_id")
+	private Profile reviewAuthor;
+
+	@ManyToOne
+	@JoinColumn(name = "profile_reviewed_id")
+	private Profile profileReviewed;
+
+}
