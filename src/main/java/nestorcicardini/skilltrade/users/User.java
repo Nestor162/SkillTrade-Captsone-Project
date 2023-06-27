5d811f6ac3d33e39273109a413991a15ad292347
Nestor162 <nestorcicardini@gmail.com>
2023-06-27 18:50:26 +0200

    Create User Class

---
+++ src/main/java/nestorcicardini/skilltrade/users/User.java
@@ -0,0 +1,39 @@
+package nestorcicardini.skilltrade.users;
+
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import nestorcicardini.skilltrade.profiles.Profile;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+	@Id
+	@GeneratedValue
+	private UUID id;
+	private String username;
+	private String email;
+	private String password;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "profile_id", referencedColumnName = "id")
+	private Profile profile;
+
+	public User(String username, String email, String password) {
+		super();
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+
+}
