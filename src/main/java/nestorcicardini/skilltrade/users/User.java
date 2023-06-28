9b121f394648357b95b822b1be41b219e515b6e5
Nestor162 <nestorcicardini@gmail.com>
2023-06-28 18:32:48 +0200

    Fix User constructors

--- src/main/java/nestorcicardini/skilltrade/users/User.java
+++ src/main/java/nestorcicardini/skilltrade/users/User.java
@@ -1,6 +1,12 @@
 package nestorcicardini.skilltrade.users;
 
+import java.util.Collection;
 import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -10,19 +16,23 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import nestorcicardini.skilltrade.profiles.Profile;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
-public class User {
+@NoArgsConstructor
+public class User implements UserDetails {
 	@Id
 	@GeneratedValue
 	private UUID id;
 	private String username;
 	private String email;
+	@JsonIgnore
 	private String password;
 	private Role role;
 
@@ -30,11 +40,44 @@ public class User {
 	@JoinColumn(name = "profile_id", referencedColumnName = "id")
 	private Profile profile;
 
-	public User(String username, String email, String password) {
+	public User(String username, String email, String password, Role role,
+			Profile profile) {
 		super();
 		this.username = username;
 		this.email = email;
 		this.password = password;
+		this.role = role;
+		this.profile = profile;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
