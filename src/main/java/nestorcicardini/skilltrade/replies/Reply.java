0ae8aca963007c4ca3e9afffc001132990d92008
Nestor162 <nestorcicardini@gmail.com>
2023-06-28 13:34:08 +0200

    Create Reply Class

---
+++ src/main/java/nestorcicardini/skilltrade/replies/Reply.java
@@ -0,0 +1,37 @@
+package nestorcicardini.skilltrade.replies;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import nestorcicardini.skilltrade.profiles.Profile;
+import nestorcicardini.skilltrade.reviews.Review;
+
+@Entity
+@Table(name = "replies")
+@Getter
+@Setter
+public class Reply {
+	@Id
+	@GeneratedValue
+	private UUID id;
+	private String content;
+	private LocalDate publicationDate;
+	private long likes;
+
+	@ManyToOne
+	@JoinColumn(name = "profile_id")
+	private Profile profile;
+
+	@ManyToOne
+	@JoinColumn(name = "review_id")
+	private Review review;
+
+}
