823995c449068aba11eebf961cbde9817bfac4d8
Nestor162 <nestorcicardini@gmail.com>
2023-06-29 14:07:50 +0200

    Add void constructor

--- src/main/java/nestorcicardini/skilltrade/auth/exceptions/InvalidTokenException.java
+++ src/main/java/nestorcicardini/skilltrade/auth/exceptions/InvalidTokenException.java
@@ -1,6 +1,9 @@
 package nestorcicardini.skilltrade.auth.exceptions;
 
+import lombok.NoArgsConstructor;
+
 @SuppressWarnings("serial")
+@NoArgsConstructor
 public class InvalidTokenException extends RuntimeException {
 	public InvalidTokenException(String message) {
 		super(message);
