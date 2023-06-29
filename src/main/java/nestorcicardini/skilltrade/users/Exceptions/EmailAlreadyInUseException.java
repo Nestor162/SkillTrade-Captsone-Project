a4a84712bf6271c135fcd93e82cdc91186944376
Nestor162 <nestorcicardini@gmail.com>
2023-06-29 10:00:51 +0200

    Delete user exceptions in order to rename package

--- src/main/java/nestorcicardini/skilltrade/users/Exceptions/EmailAlreadyInUseException.java
+++
@@ -1,9 +0,0 @@
-package nestorcicardini.skilltrade.users.exceptions;
-
-@SuppressWarnings("serial")
-public class EmailAlreadyInUseException extends RuntimeException {
-	public EmailAlreadyInUseException(String message) {
-		super(message);
-	}
-
-}
--- src/main/java/nestorcicardini/skilltrade/users/Exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package nestorcicardini.skilltrade.users.exceptions;
-
-@SuppressWarnings("serial")
-public class UserNotFoundException extends RuntimeException {
-	public UserNotFoundException(String message) {
-		super(message);
-	}
-}
