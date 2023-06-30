162c5c00c7f8f82a46ab0123b868629f4faa7438
Nestor162 <nestorcicardini@gmail.com>
2023-06-30 18:31:30 +0200

    Add language class

---
+++ src/main/java/nestorcicardini/skilltrade/languages/Language.java
@@ -0,0 +1,33 @@
+package nestorcicardini.skilltrade.languages;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import nestorcicardini.skilltrade.profiles.Profile;
+
+@Entity
+@Table(name = "langs")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Language {
+	@Id
+	private String languageCode;
+	private String languageName;
+
+	@ManyToMany(mappedBy = "spokenLanguages")
+	List<Profile> profiles;
+
+	public Language(String languageName, String languageCode) {
+		super();
+		this.languageName = languageName;
+		this.languageCode = languageCode;
+	}
+
+}
