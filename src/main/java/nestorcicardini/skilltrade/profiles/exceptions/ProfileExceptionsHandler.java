0f897db696d054aafb644bf2df7fc23acac83e9e
Nestor162 <nestorcicardini@gmail.com>
2023-07-01 13:19:35 +0200

    Update exception name

--- src/main/java/nestorcicardini/skilltrade/profiles/exceptions/ProfileExceptionsHandler.java
+++ src/main/java/nestorcicardini/skilltrade/profiles/exceptions/ProfileExceptionsHandler.java
@@ -8,14 +8,13 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import nestorcicardini.skilltrade.globalExceptions.ErrorPayload;
-import nestorcicardini.skilltrade.users.exceptions.UserNotFoundException;
 
 @ControllerAdvice
 public class ProfileExceptionsHandler {
 
 	@ExceptionHandler(ProfileNotFoundException.class)
 	public ResponseEntity<ErrorPayload> handleUserNotFoundException(
-			UserNotFoundException ex) {
+			ProfileNotFoundException ex) {
 		ErrorPayload payload = new ErrorPayload(ex.getMessage(), new Date(),
 				404);
 		return new ResponseEntity<ErrorPayload>(payload, HttpStatus.NOT_FOUND);
