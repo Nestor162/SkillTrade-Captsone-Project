e1474395bbf4d8376b0ba1d4dfb03dc1a5c0e1f6
Nestor162 <nestorcicardini@gmail.com>
2023-07-02 14:20:30 +0200

    Changed return type to optional

--- src/main/java/nestorcicardini/skilltrade/languages/LanguageRepository.java
+++ src/main/java/nestorcicardini/skilltrade/languages/LanguageRepository.java
@@ -1,9 +1,11 @@
 package nestorcicardini.skilltrade.languages;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LanguageRepository extends JpaRepository<Language, String> {
 
-	Language findByLanguageCode(String languageCode);
+	Optional<Language> findByLanguageCode(String languageCode);
 
 }
