c949a88e4ceea29e0667a0aa003744b13da9ebfc
Nestor162 <nestorcicardini@gmail.com>
2023-07-03 10:22:06 +0200

    Add void constructor

--- src/main/java/nestorcicardini/skilltrade/interests/Interest.java
+++ src/main/java/nestorcicardini/skilltrade/interests/Interest.java
@@ -6,15 +6,22 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "interests")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Interest {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+
+	public Interest(String name) {
+		this.name = name;
+	}
+
 }
