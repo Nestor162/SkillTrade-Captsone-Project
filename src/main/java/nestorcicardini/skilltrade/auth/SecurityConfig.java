8fb674896ad87edb70efc9ff973641df29b73bbb
Nestor162 <nestorcicardini@gmail.com>
2023-07-12 12:13:05 +0200

    Configure CORS to allow requests from frontend origin to all endpoints

--- src/main/java/nestorcicardini/skilltrade/auth/SecurityConfig.java
+++ src/main/java/nestorcicardini/skilltrade/auth/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -21,9 +23,20 @@ public class SecurityConfig {
 	JWTAuthFilter jwtAuthFilter;
 
 	@Bean
+	WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:5173");
+			}
+		};
+	}
+
+	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http)
 			throws Exception {
-		http.cors(c -> c.disable());
+
 		http.csrf(c -> c.disable());
 
 		http.authorizeHttpRequests(
