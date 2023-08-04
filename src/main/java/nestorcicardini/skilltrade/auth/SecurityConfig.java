492453fdb3022cd1067e13808b843b4e67def85b
Nestor162 <nestorcicardini@gmail.com>
2023-08-04 11:29:00 +0200

    Fix CORS configuration

--- src/main/java/nestorcicardini/skilltrade/auth/SecurityConfig.java
+++ src/main/java/nestorcicardini/skilltrade/auth/SecurityConfig.java
@@ -1,8 +1,11 @@
 package nestorcicardini.skilltrade.auth;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,8 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -23,24 +27,13 @@ public class SecurityConfig {
 	JWTAuthFilter jwtAuthFilter;
 
 	@Bean
-	WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("https://skilltrade.onrender.com")
-						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE",
-								"OPTIONS");
-				;
-			}
-		};
-	}
-
-	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http)
 			throws Exception {
 
 		http.csrf(c -> c.disable());
+		http.cors(Customizer.withDefaults()); // by default uses a Bean by the
+												// name of
+												// corsConfigurationSource
 
 		http.authorizeHttpRequests(
 				auth -> auth.requestMatchers("/auth/register").permitAll());
@@ -90,6 +83,18 @@ public class SecurityConfig {
 	@Bean
 	PasswordEncoder pwEncoder() {
 		return new BCryptPasswordEncoder(10);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(
+				Arrays.asList("https://skilltrade.vercel.app"));
+		configuration.setAllowedMethods(Arrays.asList("*"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 }
